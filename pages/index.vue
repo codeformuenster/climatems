0525cb6eb7ddfea81cf540a11c4a0737345bdeef
Jonas Juchim <jjuchim@googlemail.com>
2024-09-27 14:28:26 +0200

    Add categories to root page

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,14 @@
         <ProgressBarChart :chart-data="value" />
       </template>
     </Card>
+    {{ categories }}
   </div>
 </template>
 
 <script setup lang="ts">
+import actions from '@/data/b-2_actions.json';
+
+const categories = Object.keys(actions);
 
 const value = ref([
   { label: 'Umgesetzt', color: 'var(--p-green-500)', value: 16 },
@@ -21,6 +25,7 @@ const value = ref([
 <style>
 .wrapper {
   display: flex;
-  justify-content: center;
+  align-content: center;
+  flex-direction: column;
 }
 </style>
