67c3d8f10322f992d4f877dab3a0ac07af29bea2
Jonas Juchim <jjuchim@googlemail.com>
2024-09-27 20:06:26 +0200

    Add measure details

--- pages/category/[categoryId]/measure/[measureId].vue
+++ pages/category/[categoryId]/measure/[measureId].vue
@@ -1,6 +1,5 @@
 <template>
   <div class="main-content">
-
     <Card>
       <template #title>
         {{  measure?.['Action outline']['Action name'] }}
@@ -19,14 +18,28 @@
         <div v-if="progress?.measure === 'binary'">
           {{ progress?.status === 'completed' ? 'Erreicht' : 'Nicht erreicht' }}
         </div>
-
-        <!-- <p class="prose prose-">
-          {{ measure?.['Action outline']['Action description'] }}
-        </p> -->
-        
       </template>
     </Card>
-    {{ JSON.stringify(measure)}}
+
+    <template v-for="k in ['Action outline', 'Reference to impact pathway', 'Implementation', 'Impact & cost']">
+      <div class="overflow-hidden bg-white shadow sm:rounded-lg">
+        <div class="px-4 py-6 sm:px-6">
+          <h3 class="text-base font-semibold leading-7 text-gray-900">{{ k }}</h3>
+          <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">weitere Informationen</p>
+        </div>
+        <div class="border-t border-gray-100">
+          <dl class="divide-y divide-gray-100">
+            
+            <template v-for="value, key in measure?.[k]">
+              <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-900">{{key}}</dt>
+                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{value}}</dd>
+              </div>
+            </template>
+          </dl>
+        </div>
+      </div>
+    </template> 
   </div>
 </template>
 
