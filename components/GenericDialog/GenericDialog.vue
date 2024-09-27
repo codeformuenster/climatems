da00c70c6703a915ccd6772bb90a89f4256e20a8
Merlin Famula <merlin.famula@gmail.com>
2024-09-27 16:47:32 +0200

    Add generic dialog & alert button to open it

---
+++ components/GenericDialog/GenericDialog.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageHeader",
+  props: {
+    visible: {
+      value: Boolean,
+      required: true,
+    },
+    onClose: {
+      type: Function,
+      required: true,
+    },
+    title: {
+      text: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <Dialog
+    v-model:visible="visible"
+    modal
+    :header="title"
+    :style="{ width: '25rem' }"
+  >
+    <span class="p-text-secondary block mb-5">{{ description }}</span>
+    <div class="flex align-items-center gap-3 mb-5">
+      <label for="email" class="font-semibold w-6rem">Email</label>
+      <InputText id="email" class="flex-auto" autocomplete="off" />
+    </div>
+    <div class="flex justify-content-end gap-2">
+      <Button
+        type="button"
+        label="Cancel"
+        severity="secondary"
+        @click="onClose"
+      ></Button>
+      <Button type="button" label="Save" @click="onClose"></Button>
+    </div>
+  </Dialog>
+</template>
