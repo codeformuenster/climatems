0ad9bb7c36a1b5faaac93794980a98084ad398e8
Tobias Reischmann <reischmann@energielenker.de>
2024-09-27 21:41:43 +0200

    Insert newlines for measurefields

--- pages/category/[categoryId]/measure/[measureId].vue
+++ pages/category/[categoryId]/measure/[measureId].vue
@@ -66,7 +66,7 @@
             <template v-for="value, key in measure?.[k]">
               <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt class="text-sm font-medium text-gray-900">{{key}}</dt>
-                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{value}}</dd>
+                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0" v-html="value.replaceAll('\n', '<br/>')"></dd>
               </div>
             </template>
           </dl>
