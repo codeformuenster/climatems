f92bc42025021372f29be1be34ff8fc89e71d075
Jonas Juchim <jjuchim@googlemail.com>
2024-09-27 12:18:19 +0200

    Add prime-vue and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
+import Aura from '@primevue/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module',
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
+  }
 })
