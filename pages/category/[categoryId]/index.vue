cf082c678e7f09696d8a3234ca1610c2a7ff085f
Jonas Juchim <jjuchim@googlemail.com>
2024-09-27 19:18:10 +0200

    Load category on detail page

--- pages/category/[categoryId]/index.vue
+++ pages/category/[categoryId]/index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    Category
-    {{ $route.params.categoryId }}
+  <div class="main-content">
+   {{ JSON.stringify(category) }}
   </div>
 </template>
+
+<script lang="ts" setup>
+import { getCategory } from '~/dataProcessing/loadData';
+
+const route = useRoute();
+
+const category = await getCategory(route.params.categoryId);
+</script>
