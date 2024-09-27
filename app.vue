b52e0966efb4d650cbdf3a4bd84ffa8fd9f4ba12
Jonas Juchim <jjuchim@googlemail.com>
2024-09-27 12:48:33 +0200

    Add very simple overview chart

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="app">
+    <div class="wrapper">
+      <Card>
+        <template #content>
+          <ProgressBarChart :chart-data="value" />
+        </template>
+      </Card>
+    </div>
+
+  </main>
+</template>
+
+<script setup lang="ts">
+
+const value = ref([
+  { label: 'Umgesetzt', color: 'var(--p-green-500)', value: 16 },
+  { label: 'Begonnen', color: 'var(--p-yellow-500)', value: 8 },
+  { label: 'nicht begonnen', color: 'var(--p-grey-500)', value: 24 },
+  { label: 'verschoben', color: 'var(--p-red-500)', value: 10 }
+]);
+</script>
+
+<style>
+.app {
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--p-gray-50);
+}
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ components/ProgressBarChart.vue
@@ -0,0 +1,21 @@
+<template>
+  <MeterGroup :value="chartData" :max="sum" />
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+
+interface ChartData {
+  labels: string;
+  color: string;
+  value: number;
+}
+
+const props = defineProps<{
+  chartData: ChartData[];
+}>();
+
+const sum = computed(() => {
+  return props.chartData.reduce((acc, item) => acc + item.value, 0);
+});
+</script>
