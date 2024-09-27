46458da9baf7b2fb2aa5bd8196e771aa7ba62a3b
Merlin Famula <merlin.famula@gmail.com>
2024-09-27 19:38:07 +0200

    Add pageHeader to category

--- pages/category/[categoryId]/index.vue
+++ pages/category/[categoryId]/index.vue
@@ -1,10 +1,15 @@
 <template>
+  <PageHeader
+  imageSrc="https://westerwaldkreis.klimaschutzportal.rlp.de/fileadmin/_processed_/b/8/csm_wwkreis_umwelt_logo_neu_rgb_a3cc7eddd8.png"
+  :title="category?.name"
+  />
   <div class="main-content">
-   {{ JSON.stringify(category) }}
+  {{ JSON.stringify(category) }}
   </div>
 </template>
 
 <script lang="ts" setup>
+import PageHeader from '~/components/PageHeader.vue';
 import { getCategory } from '~/dataProcessing/loadData';
 
 const route = useRoute();
