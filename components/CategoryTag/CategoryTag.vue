70eb6e2b0eb4e0313ec6830b38ea4667432d3ed4
Annika Limbrock <a.limbrock@web.de>
2024-09-28 16:52:45 +0200

    Adjust measure index page and headline and add category-tag

---
+++ components/CategoryTag/CategoryTag.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { getCategoryIcon } from '~/helper';
+
+const props = defineProps<{
+  title: string;
+  categoryId: string;
+}>();
+</script>
+
+<template>
+  <span class="category-tag" :data-is-category="categoryId">
+    <span class="category-tag--icon">
+      <component :is="getCategoryIcon(categoryId)" />
+    </span>
+    <span class="category-tag--text">{{ title }}</span>
+  </span>
+</template>
+
+<style scoped>
+@import "CategoryTag.css";
+</style>
--- pages/category/[categoryId]/measure/[measureId].vue
+++ pages/category/[categoryId]/measure/[measureId].vue
@@ -1,39 +1,44 @@
 <template>
   <Breadcrumbs :items="items"/>
-  <Card>
-    <template #title>
+  <section class="container">
+    <h1 class="headline" :data-is-measure="true">
       {{ measure?.additionalData?.short_title || measure?.original['Action outline']['Action name'] }}
-      <Tag :value="measure?.category"/>
-    </template>
-    <template #content>
-      <p class="prosa">
-        {{ measure?.additionalData?.summary }}
-      </p>
-      <div v-if="measure?.progress?.type === 'percent'">
-        <Chart type="line" :data="chartData" :options="chartOptions" class="h-[30rem]"/>
-      </div>
-      <div v-if="measure?.progress?.type === 'count'">
-        <Chart type="line" :data="chartData" :options="chartOptions" class="h-[30rem]"/>
-      </div>
-      <div v-if="measure?.progress?.type === 'percent'">
-        <MeterGroup :value="[{
-          label: 'erreicht',
-          value: measure?.progress?.values[measure?.progress?.values.length - 1].value,
-          color: 'var(--p-green-500)'
-          } ]"/>
-        {{ measure?.progress?.values[measure?.progress?.values.length - 1].value }} % erreicht
-      </div>
+      <CategoryTag :title="measure?.category || ''" :category-id="measure?.category || ''" />
+    </h1>
+    <Card>
+      <template #title>
+        {{ measure?.additionalData?.short_title || measure?.original['Action outline']['Action name'] }}
+        <Tag :value="measure?.category"/>
+      </template>
+      <template #content>
+        <p class="prosa">
+          {{ measure?.additionalData?.summary }}
+        </p>
+        <div v-if="measure?.progress?.type === 'percent'">
+          <Chart type="line" :data="chartData" :options="chartOptions" class="h-[30rem]"/>
+        </div>
+        <div v-if="measure?.progress?.type === 'count'">
+          <Chart type="line" :data="chartData" :options="chartOptions" class="h-[30rem]"/>
+        </div>
+        <div v-if="measure?.progress?.type === 'percent'">
+          <MeterGroup :value="[{
+            label: 'erreicht',
+            value: measure?.progress?.values[measure?.progress?.values.length - 1].value,
+            color: 'var(--p-green-500)'
+            } ]"/>
+          {{ measure?.progress?.values[measure?.progress?.values.length - 1].value }} % erreicht
+        </div>
 
-      <div v-if="measure?.progress?.type === 'binary'">
-        Aktueller Status:
-        {{
-          measure?.progress?.values[measure?.progress?.values.length - 1]?.value === 'completed' ? 'Erreicht' : 'Nicht erreicht'
-        }}
-      </div>
-    </template>
-  </Card>
-
-  <template v-if="!!measure?.additionalData?.user_action">
+        <div v-if="measure?.progress?.type === 'binary'">
+          Aktueller Status:
+          {{
+            measure?.progress?.values[measure?.progress?.values.length - 1]?.value === 'completed' ? 'Erreicht' : 'Nicht erreicht'
+          }}
+        </div>
+      </template>
+    </Card>
+  </section>
+  <section class="section container" v-if="!!measure?.additionalData?.user_action">
     <div id="alert-additional-content-1"
           class="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
           role="alert">
@@ -61,159 +66,164 @@
         </a>
       </div>
     </div>
-  </template>
-
-  <div class="overflow-hidden bg-white shadow sm:rounded-lg mb-6">
-    <div class="px-4 py-6 sm:px-6">
-      <h3 class="text-base font-semibold leading-7 text-gray-900">Maßnahmen-Details</h3>
-      <p class="mt-1 max-w-6xl text-sm leading-6 text-gray-500">
-        Darum geht es
-      </p>
-    </div>
-    <div class="border-t border-gray-100">
-      <dl class="divide-y divide-gray-100">
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Worum geht es?
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            {{ measure?.original?.['Action outline']['Action name'] }}
-          </dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Was wird im Detail geplant?
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
-              v-html="measure?.original?.['Action outline']['Action description'].replaceAll('\n', '<br/>')"></dd>
-        </div>
-      </dl>
-    </div>
-  </div>
-
-  <div class="overflow-hidden bg-white shadow sm:rounded-lg mb-6">
-    <div class="px-4 py-6 sm:px-6">
-      <h3 class="text-base font-semibold leading-7 text-gray-900">Auswirkungen</h3>
-      <p class="mt-1 max-w-6xl text-sm leading-6 text-gray-500">
-        Fakten die erreicht werden wollen
-      </p>
-    </div>
-    <div class="border-t border-gray-100">
-      <dl class="divide-y divide-gray-100">
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6" v-if="measure?.original?.['Impact & cost']['Generated renewable energy (if applicable)'] !==''">
-          <dt class="text-sm font-medium text-gray-900">
-            Neu generierte erneuerbare Energien
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            {{ measure?.original?.['Impact & cost']['Generated renewable energy (if applicable)'] }}
-          </dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Welcher Energieträger wurde reduziert/ersetzt?
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            {{ measure?.original?.['Impact & cost']['Removed/substituted energy, volume, or fuel type'] }}
-          </dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Geschätzte potentielle CO2-Einsparung
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
-              v-html="measure?.original?.['Impact & cost']['GHG emissions reduction estimate (total) per emission source sector']?.replaceAll('\n', '<br/>')"></dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Kosten der Maßnahme
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            {{ measure?.additionalData?.cost.value === null ? "keine Angabe/trifft nicht zu" : formatNumber(measure?.additionalData?.cost.value).concat(" €")}}
-          </dd>
-        </div>
-      </dl>
-    </div>
-  </div>
-
-  <div class="overflow-hidden bg-white shadow sm:rounded-lg mb-6">
-    <div class="px-4 py-6 sm:px-6">
-      <h3 class="text-base font-semibold leading-7 text-gray-900">Umsetzung</h3>
-      <p class="mt-1 max-w-6xl text-sm leading-6 text-gray-500">
-        Details zur Umsetzung der Maßnahme
-      </p>
-    </div>
-    <div class="border-t border-gray-100">
-      <dl class="divide-y divide-gray-100">
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Welchen Bereich betrifft die Maßnahme?
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            {{ measure?.original?.['Implementation']['Action scale & addressed entities'] }}
-          </dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Wer macht es?
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            {{ measure?.original?.['Implementation']['Responsible bodies/person for implementation'] }}
-          </dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Wer ist noch beteiligt?
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
-              v-html="measure?.original?.['Implementation']['Involved stakeholders'].replaceAll('\n', '<br/>')"></dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Geplante Umsetzung bis
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            {{ measure?.additionalData?.cost.until_in_years }}
-          </dd>
-        </div>
-        <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-900">
-            Was sind die konkreten Schritte/Fortschritte?
-          </dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
-              v-html="measure?.original?.['Implementation']['Comments on implementation'].replaceAll('\n', '<br/>')"></dd>
-        </div>
-      </dl>
-    </div>
-  </div>
-  <div class="rohdaten-accordion">
-    <div class="flex mb-4 gap-2">
+  </section>
+  <section class="section container">
+    <h1 class="headline">Maßnahmen-Details</h1>
+    <div class="overflow-hidden bg-white shadow sm:rounded-lg mb-6">
       <div class="px-4 py-6 sm:px-6">
-        <h3 class="text-base font-semibold leading-7 text-gray-900">Für tiefer-Interessierte</h3>
-        <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Rohdaten aus dem Klimastadtvertrag</p>
+        <h3 class="text-base font-semibold leading-7 text-gray-900">Beschreibung</h3>
+        <p class="mt-1 max-w-6xl text-sm leading-6 text-gray-500">
+          Darum geht es
+        </p>
+      </div>
+      <div class="border-t border-gray-100">
+        <dl class="divide-y divide-gray-100">
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Worum geht es?
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+              {{ measure?.original?.['Action outline']['Action name'] }}
+            </dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Was wird im Detail geplant?
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
+                v-html="measure?.original?.['Action outline']['Action description'].replaceAll('\n', '<br/>')"></dd>
+          </div>
+        </dl>
       </div>
     </div>
-    <Accordion :value="[]" multiple>
-      <template v-for="(k, index) in ['Action outline', 'Reference to impact pathway', 'Implementation', 'Impact & cost']">
-        <AccordionPanel :value="index">
+    <div class="overflow-hidden bg-white shadow sm:rounded-lg mb-6">
+      <div class="px-4 py-6 sm:px-6">
+        <h3 class="text-base font-semibold leading-7 text-gray-900">Auswirkungen</h3>
+        <p class="mt-1 max-w-6xl text-sm leading-6 text-gray-500">
+          Fakten die erreicht werden wollen
+        </p>
+      </div>
+      <div class="border-t border-gray-100">
+        <dl class="divide-y divide-gray-100">
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6" v-if="measure?.original?.['Impact & cost']['Generated renewable energy (if applicable)'] !==''">
+            <dt class="text-sm font-medium text-gray-900">
+              Neu generierte erneuerbare Energien
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+              {{ measure?.original?.['Impact & cost']['Generated renewable energy (if applicable)'] }}
+            </dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Welcher Energieträger wurde reduziert/ersetzt?
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+              {{ measure?.original?.['Impact & cost']['Removed/substituted energy, volume, or fuel type'] }}
+            </dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Geschätzte potentielle CO2-Einsparung
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
+                v-html="measure?.original?.['Impact & cost']['GHG emissions reduction estimate (total) per emission source sector']?.replaceAll('\n', '<br/>')"></dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Kosten der Maßnahme
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+              {{ measure?.additionalData?.cost.value === null ? "keine Angabe/trifft nicht zu" : formatNumber(measure?.additionalData?.cost.value).concat(" €")}}
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+    <div class="overflow-hidden bg-white shadow sm:rounded-lg mb-6">
+      <div class="px-4 py-6 sm:px-6">
+        <h3 class="text-base font-semibold leading-7 text-gray-900">Umsetzung</h3>
+        <p class="mt-1 max-w-6xl text-sm leading-6 text-gray-500">
+          Details zur Umsetzung der Maßnahme
+        </p>
+      </div>
+      <div class="border-t border-gray-100">
+        <dl class="divide-y divide-gray-100">
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Welchen Bereich betrifft die Maßnahme?
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+              {{ measure?.original?.['Implementation']['Action scale & addressed entities'] }}
+            </dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Wer macht es?
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+              {{ measure?.original?.['Implementation']['Responsible bodies/person for implementation'] }}
+            </dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Wer ist noch beteiligt?
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
+                v-html="measure?.original?.['Implementation']['Involved stakeholders'].replaceAll('\n', '<br/>')"></dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Geplante Umsetzung bis
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+              {{ measure?.additionalData?.cost.until_in_years }}
+            </dd>
+          </div>
+          <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-900">
+              Was sind die konkreten Schritte/Fortschritte?
+            </dt>
+            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0"
+                v-html="measure?.original?.['Implementation']['Comments on implementation'].replaceAll('\n', '<br/>')"></dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </section>
+  <section class="section container">
+    <h1 class="headline">Weitere Informationen</h1>
+    <div class="rohdaten-accordion">
+      <div class="flex mb-4 gap-2">
+        <div class="px-4 py-6 sm:px-6">
+          <h3 class="text-base font-semibold leading-7 text-gray-900">Rohdaten aus dem Klimastadtvertrag</h3>
+          <p class="mt-1 max-w-6xl text-sm leading-6 text-gray-500">
+            Für alle, die es genauer wissen wollen
+          </p>
+        </div>
+      </div>
+      <Accordion :value="[]" multiple>
+        <template v-for="(k, index) in ['Action outline', 'Reference to impact pathway', 'Implementation', 'Impact & cost']">
+          <AccordionPanel :value="index">
 
-          <AccordionHeader>{{ k }}</AccordionHeader>
-          <AccordionContent>
-            <div class="border-t border-gray-100">
-              <dl class="divide-y divide-gray-100">
-                <template v-for="(value, key) in measure?.original?.[k]">
-                  <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt class="text-sm font-medium text-gray-900">{{key}}</dt>
-                    <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0" v-html="value.replaceAll('\n', '<br/>')"></dd>
-                  </div>
-                </template>
-              </dl>
-            </div>
-          </AccordionContent>
-        </AccordionPanel>
-      </template>
+            <AccordionHeader>{{ k }}</AccordionHeader>
+            <AccordionContent>
+              <div class="border-t border-gray-100">
+                <dl class="divide-y divide-gray-100">
+                  <template v-for="(value, key) in measure?.original?.[k]">
+                    <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                      <dt class="text-sm font-medium text-gray-900">{{key}}</dt>
+                      <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0" v-html="value.replaceAll('\n', '<br/>')"></dd>
+                    </div>
+                  </template>
+                </dl>
+              </div>
+            </AccordionContent>
+          </AccordionPanel>
+        </template>
 
-    </Accordion>
-  </div>
+      </Accordion>
+    </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
@@ -221,7 +231,7 @@ import Chart from 'primevue/chart';
 import { getMeasure } from '~/dataProcessing/loadData';
 import annotationPlugin from 'chartjs-plugin-annotation';
 import { Chart as ChartJS, registerables } from 'chart.js';
-import { Breadcrumbs } from '~/components';
+import { Breadcrumbs, CategoryTag } from '~/components';
 import Accordion from 'primevue/accordion';
 import AccordionPanel from 'primevue/accordionpanel';
 import AccordionHeader from 'primevue/accordionheader';
@@ -234,13 +244,9 @@ const route = useRoute();
 
 const measure = await getMeasure(route.params.measureId as string);
 
-const home = ref({
-    icon: 'pi pi-home',
-    route: '/'
-});
 const items = ref([
-    { label: measure?.category, route: `/category/${measure?.categoryId}`, icon: 'pi pi-tag' },
-    { label: measure?.additionalData?.short_title || measure?.original['Action outline']['Action name'], icon: 'pi pi-tag' },
+    { label: measure?.category, route: `/category/${measure?.categoryId}` },
+    { label: measure?.additionalData?.short_title || measure?.original['Action outline']['Action name'] },
 ]);
 const chartData = {
   datasets: [
