27f53cc91241762a5ea5a6f8c34bbaa29e491ecb
Annika Limbrock <a.limbrock@web.de>
2024-09-28 12:30:09 +0200

    Fix measure-id page

--- pages/category/[categoryId]/measure/[measureId].vue
+++ pages/category/[categoryId]/measure/[measureId].vue
@@ -1,5 +1,5 @@
 <template>
-  <Breadcrumbs :items="items"/> 
+  <Breadcrumbs :items="items"/>
   <Card>
     <template #title>
       {{ measure?.additionalData?.short_title || measure?.original['Action outline']['Action name'] }}
@@ -33,15 +33,29 @@
     </template>
   </Card>
 
-    <template v-if="!!measure?.additionalData?.user_action">
-      <div id="alert-additional-content-1"
-           class="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
-           role="alert">
-        <div class="flex items-center">
-          <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-               fill="currentColor" viewBox="0 0 20 20">
-            <path
-                d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+  <template v-if="!!measure?.additionalData?.user_action">
+    <div id="alert-additional-content-1"
+          class="p-4 mb-4 text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+          role="alert">
+      <div class="flex items-center">
+        <svg class="flex-shrink-0 w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor" viewBox="0 0 20 20">
+          <path
+              d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <span class="sr-only">Info</span>
+        <h3 class="text-lg font-medium">Werde selber aktiv</h3>
+      </div>
+      <div class="mt-2 mb-4 text-sm">
+        {{ measure?.additionalData?.user_action.description }}
+      </div>
+      <div class="flex">
+        <a
+          class="text-white bg-blue-800 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-xs px-3 py-1.5 me-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          :href="measure?.additionalData?.user_action.href"
+        >
+          <svg class="me-2 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 14">
+            <path d="M10 0C4.612 0 0 5.336 0 7c0 1.742 3.546 7 10 7 6.454 0 10-5.258 10-7 0-1.664-4.612-7-10-7Zm0 10a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z"/>
           </svg>
           {{ measure?.additionalData?.user_action.label }}
         </a>
@@ -72,19 +86,23 @@
 
 <script lang="ts" setup>
 import Chart from 'primevue/chart';
-import { Breadcrumbs } from '~/components';
 import { getMeasure, getProgressForMeasure } from '~/dataProcessing/loadData';
 import annotationPlugin from 'chartjs-plugin-annotation';
 import { Chart as ChartJS, registerables } from 'chart.js';
+import { Breadcrumbs } from '~/components';
 
 ChartJS.register(...registerables, annotationPlugin);
 const route = useRoute();
 
 const measure = await getMeasure(route.params.measureId as string);
 
+const home = ref({
+    icon: 'pi pi-home',
+    route: '/'
+});
 const items = ref([
-    { label: measure?.category, route: `/category/${measure?.categoryId}` },
-    { label: measure?.additionalData?.short_title || measure?.original['Action outline']['Action name'] },
+    { label: measure?.category, route: `/category/${measure?.categoryId}`, icon: 'pi pi-tag' },
+    { label: measure?.additionalData?.short_title || measure?.original['Action outline']['Action name'], icon: 'pi pi-tag' },
 ]);
 const chartData = {
   labels: ["2021-01-01", "2021-04-01", "2021-07-01", "2021-10-01", "2022-01-01", "2022-04-01", "2022-07-01", "2022-10-01", "2023-01-01", "2023-04-01", "2023-07-01", "2023-10-01", "2024-01-01", "2024-04-01", "2024-07-01", "2024-10-01"],
@@ -166,3 +184,11 @@ if (measure?.progress?.type === 'count') {
   }
 }
 </script>
+
+<style>
+.main-content {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
